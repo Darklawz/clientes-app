5963c16605acf599e9b157873490177d521fc58b
Rigoberto Castillo <rigobertobonilla95@gmail.com>
2022-01-20 23:35:52 -0600

    Prueba

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,12 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[]  = [
+  {id: 1, nombre:'Andres', apellido:'Guzman', email:'profesor@bolsadeideas.com', createAt:'2017-12-11'},
+  {id: 2, nombre:'Juan', apellido:'Guzman', email:'profesor@bolsadeideas.com', createAt:'2018-12-11'},
+  {id: 3, nombre:'Jose', apellido:'Guzman', email:'profesor@bolsadeideas.com', createAt:'2019-12-11'},
+  {id: 4, nombre:'Luis', apellido:'Guzman', email:'profesor@bolsadeideas.com', createAt:'2016-12-11'},
+  {id: 5, nombre:'Rigo', apellido:'Guzman', email:'profesor@bolsadeideas.com', createAt:'2015-12-11'},
+  {id: 6, nombre:'Jordan', apellido:'Guzman', email:'profesor@bolsadeideas.com', createAt:'2013-12-11'},
+  {id: 7, nombre:'Endy', apellido:'Guzman', email:'profesor@bolsadeideas.com', createAt:'2012-12-11'},
+  {id: 8, nombre:'Raul', apellido:'Guzman', email:'profesor@bolsadeideas.com', createAt:'2011-12-11'}
+];
